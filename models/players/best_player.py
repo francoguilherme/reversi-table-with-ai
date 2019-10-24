a4c831f3c4b7d254fa03becebfe3b584c0cc54d5
Guilherme Franco <guilherme.franco@pigpeg.com>
2019-10-23 22:31:28 -0300

    Calculating best moves

--- models/players/best_player.py
+++ models/players/best_player.py
@@ -3,7 +3,7 @@ from models.playNode import PlayNode
 from models.move import Move
 
 class BestPlayer:
-  MAX_DEPTH = 4
+  MAX_DEPTH = 3
 
   def __init__(self, color):
     self.color = color
@@ -11,21 +11,36 @@ class BestPlayer:
   def play(self, board):
     import time
 
-    root = PlayNode(name="root", color="")
-
     start_time = time.time()
+
+    root = PlayNode(name="root", color="")    
     self.generateTree(board, root)
+    
+    if self.color == board.BLACK:
+      root.value = self.negamaxAlfaBeta(root, -float("inf"), float("inf"), 1)
+    elif self.color == board.WHITE:
+      root.value = -self.negamaxAlfaBeta(root, -float("inf"), float("inf"), -1)
+
+    candidates=[child.move for child in root.children if child.value == root.value]
+    bestMove = self.getNearestCorner(candidates)
+
     elapsed_time = time.time() - start_time
-    print "Total time:", elapsed_time
 
     #for pre, fill, node in RenderTree(root):
-    #  print "%s%s" % (pre, node.name)
+    #  if node==root:
+    #    print "%s%s = %f" % (pre, node.name, node.value)
+    #  else:
+    #    if node.value != None:
+    #      print "%s%s_X:%d_Y:%d = %f" % (pre, node.color, node.move.x, node.move.y, node.value)
+    #    else:
+    #      print "%s%s_X:%d_Y:%d" % (pre, node.color, node.move.x, node.move.y)
+    print "Total time:", elapsed_time
 
-    return self.getNearestCorner(board.valid_moves(self.color))
+    return bestMove
 
   def generateTree(self, board, root):
     if root.depth >= self.MAX_DEPTH:
-      self.heuristic(board)
+      root.value = self.heuristic(board)
       return
 
     #Save current player color and switch to generate opponent moves
@@ -36,7 +51,7 @@ class BestPlayer:
     for move in board.valid_moves(current):
       if move.x == previousMove[0] and move.y == previousMove[1]:
         continue
-      node = PlayNode(name="%s_X:%d_Y:%d" % (current, move.x, move.y), color="", move=move, parent=root)
+      node = PlayNode(color=self.color, move=move, parent=root)
       previousMove[0] = move.x
       previousMove[1] = move.y
       clone = board.get_clone()
@@ -46,21 +61,23 @@ class BestPlayer:
     #After generating opponent moves, return original color
     self.color = board._opponent(self.color)
 
-  def getNearestCorner(self, moves):
-    import math
-    corners = [[1,1],[1,8], [8,1], [8,8]]
-    minDist = 10
-    retMove = None
-    for move in moves:
-      for corner in corners:
-        distX = abs(corner[0] - move.x)
-        distY = abs(corner[1] - move.y)
-        dist  = math.sqrt(distX*distX + distY*distY)
-        if dist < minDist:
-          minDist = dist
-          retMove = move
+  def negamaxAlfaBeta(self, node, alfa, beta, player):
 
-    return retMove
+    if node.depth == self.MAX_DEPTH:
+      return player * node.value
+    value = -float("inf")
+
+    if node.children != None:
+      for child in node.children:
+        value = max(value, -self.negamaxAlfaBeta(child, -beta, -alfa, -player))
+        alfa = max(alfa, value)
+        
+        if alfa >= beta:
+          break
+    
+    if node.depth == 1:
+      node.value = player * value
+    return value
 
   def heuristic(self, board):
     my_color = self.color
@@ -172,4 +189,34 @@ class BestPlayer:
     score = (10 * p) + (801.724 * c) + (382.026 * l) + (78.922 * m)
     return score
 
-  
+  def getNearestCorner(self, moves):
+    import math
+    corners = [[1,1],[1,8], [8,1], [8,8]]
+    minDist = 10
+    retMove = None
+    for move in moves:
+      for corner in corners:
+        distX = abs(corner[0] - move.x)
+        distY = abs(corner[1] - move.y)
+        dist  = math.sqrt(distX*distX + distY*distY)
+        if dist < minDist:
+          minDist = dist
+          retMove = move
+
+    return retMove
+
+  def getNearestCorner(self, moves):
+    import math
+    corners = [[1,1],[1,8], [8,1], [8,8]]
+    minDist = 10
+    retMove = None
+    for move in moves:
+      for corner in corners:
+        distX = abs(corner[0] - move.x)
+        distY = abs(corner[1] - move.y)
+        dist  = math.sqrt(distX*distX + distY*distY)
+        if dist < minDist:
+          minDist = dist
+          retMove = move
+
+    return retMove
